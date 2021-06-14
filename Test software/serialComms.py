ad9f869e82a803ed83b3b53efd4d83d4439ed0ef
Edson Matsuda <edson.matsuda@hotmail.com>
2021-06-14 21:13:30 +1000

    Test software

---
+++ Test software/serialComms.py
@@ -0,0 +1,21 @@
+import serial
+
+serialPort = serial.Serial(port="COM4", baudrate=115200)
+
+serialString = ""                           # Used to hold data coming over UART
+
+
+while(1):
+
+    # Wait until there is data waiting in the serial buffer
+    if(serialPort.in_waiting > 0):
+
+        # Read data out of the buffer until a carraige return / new line is found
+        serialString = serialPort.readline()
+
+        # Print the contents of the serial data
+        print(serialString.decode('Ascii'))
+
+        # Tell the device connected over the serial port that we recevied the data!
+        # The b at the beginning is used to indicate bytes!
+        serialPort.write(b"Thank you for sending data \r\n")
