b78c352235a259a0bfb03177358533c5f9f318d9
Edson Matsuda <edson.matsuda@hotmail.com>
2022-06-08 16:02:14 +1000

    example added

---
+++ OOP/main.py
@@ -0,0 +1,5 @@
+#Import python program with the function serial_ports
+from SerialComms import serial_ports
+
+#Use the values returned from the other function
+print(serial_ports())
---
+++ Test software/test.py
@@ -0,0 +1,3 @@
+import serial
+
+serialPort = serial.Serial(port='COM8', baudrate=9600, bytesize=8, parity='N')
